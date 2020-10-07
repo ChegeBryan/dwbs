f5632ad9d3ed9ce38935294b517a9912e922acea
Chege Bryan <cbbryan8@gmail.com>
2020-10-07 17:50:21 +0300

    Add a employer middleware route test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('employer/home', 'HomeController@employerHome')->name('employer.home')->middleware('employer');
