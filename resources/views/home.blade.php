afcf7662d0d07cf71a20d26bb453e6641ae3e3d2
Chege Bryan <cbbryan8@gmail.com>
2020-10-08 09:29:53 +0300

    Add initial home page job search form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,45 @@
 @extends('layouts.app')
+@section('custom_css')
+<style>
+.bg {
+  background-image: url("{{asset('img/bg.jpeg')}}");
+  height: 80vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
+@endsection
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
+<div class="container-fluid">
+  <div class="row justify-content-center align-items-center bg">
     <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">Dashboard</div>
-
-        <div class="card-body">
-          @if (session('status'))
-          <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-          </div>
-          @endif
-
-          Home
+      <h1>Find your preferred domestic work in minutes.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.</p>
+      <form>
+        <div class="form-group">
+          <input type="text" class="form-control" id="keywords" placeholder="Keyword e.g. (Job Title, Description)">
         </div>
-      </div>
+        <div class="form-row">
+          <div class="form-group col">
+            <select id="category" class="custom-select">
+              <option selected disabled>Category</option>
+              <option>Househelp boy</option>
+              <option>Shamba boy</option>
+            </select>
+          </div>
+          <div class="form-group col">
+            <select id="location" class="custom-select">
+              <option selected>Select location</option>
+              <option>Location 1</option>
+              <option>Location 2</option>
+            </select>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </div>
