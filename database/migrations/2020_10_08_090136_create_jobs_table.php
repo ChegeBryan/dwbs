f4b1c018089bd1880d31c06ca136dc06fa67d385
Chege Bryan <cbbryan8@gmail.com>
2020-10-08 12:15:34 +0300

    Add jobs table migration file

---
+++ database/migrations/2020_10_08_090136_create_jobs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('employer_id');
+            $table->string('category');
+            $table->string('title'); // job title
+            $table->string('type'); // fulltime/parttime
+            $table->double('compensation', 8, 2); // salary
+            $table->string('county');
+            $table->string('town');
+            $table->timestamps();
+            $table->foreign('employer_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
