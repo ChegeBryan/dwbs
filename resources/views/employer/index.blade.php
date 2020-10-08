da8400ce43849285fc3b445fd9b4e1b0a15d5838
Chege Bryan <cbbryan8@gmail.com>
2020-10-08 20:22:24 +0300

    Move repeated markup to sidenav

--- resources/views/employer/index.blade.php
+++ resources/views/employer/index.blade.php
@@ -4,12 +4,7 @@
 <div class="container py-4">
   <div class="row justify-content-center">
     <div class="col-md-3">
-      <div class="card">
-        <div class="card-body">
-          <h1 class="card-title">User name</h1>
-        </div>
-        @include('employer.partials.sidenav')
-      </div>
+      @include('employer.partials.sidenav')
     </div>
     <div class="col-md-9">
       <div class="card">
--- resources/views/employer/partials/sidenav.blade.php
+++ resources/views/employer/partials/sidenav.blade.php
@@ -1,16 +1,18 @@
-<ul class="list-group list-group-flush">
-  <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"> <i
-         class="fas fa-tachometer-alt fa-fw mx-2"></i> Dashboard</a>
-  </li>
-  <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
-         class="fas fa-edit fa-fw mx-2"></i> Edit
-      Profile</a></li>
-  <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
-         class="fas fa-user-plus fa-fw mx-2"></i>
-      Post New Job</a></li>
-  <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
-         class="fas fa-briefcase fa-fw mx-2"></i>
-      Manage Jobs</a></li>
-  <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
-         class="fas fa-clipboard-list fa-fw mx-2"></i> Applications</a></li>
-</ul>
+<div class="card">
+  <div class="card-body">
+    <h1 class="card-title">User name</h1>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"> <i
+           class="fas fa-tachometer-alt fa-fw mx-2"></i> Dashboard</a>
+    </li>
+    <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
+           class="fas fa-edit fa-fw mx-2"></i> Edit Profile</a></li>
+    <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
+           class="fas fa-user-plus fa-fw mx-2"></i> Post New Job</a></li>
+    <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
+           class="fas fa-briefcase fa-fw mx-2"></i> Manage Jobs</a></li>
+    <li class="list-group-item list-group-item-action"><a class="pl-2 stretched-link text-decoration-none" href="#"><i
+           class="fas fa-clipboard-list fa-fw mx-2"></i> Applications</a></li>
+  </ul>
+</div>
