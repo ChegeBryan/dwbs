3f921f9cfa0280ffe05a12f956a5ed0cb9f22ec0
Chege Bryan <cbbryan8@gmail.com>
2020-10-08 10:14:41 +0300

    Style registration form submit button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,13 +65,10 @@
                    autocomplete="new-password">
           </div>
 
-          <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-              <button type="submit" class="btn btn-primary">
-                {{ __('Register') }}
-              </button>
-            </div>
-          </div>
+
+          <button type="submit" class="btn btn-primary btn-block">
+            {{ __('Register') }}
+          </button>
         </form>
       </div>
     </div>
