3f19c11095dbb996aab4aa3fb32b20b560b65ced
Chege Bryan <cbbryan8@gmail.com>
2020-10-09 11:17:44 +0300

    Fix randomelement array

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -8,9 +8,9 @@ use Faker\Generator as Faker;
 $factory->define(Job::class, function (Faker $faker) {
     return [
         'employer_id' => 1,
-        'category' => $faker->randomElement('Shamba Boy', 'House help'),
+        'category' => $faker->randomElement($array = array('Shamba Boy', 'House help')),
         'title' => $faker->word,
-        'type' => $faker->randomElement('Full Time', 'Part Time'),
+        'type' => $faker->randomElement($array = array('Full Time', 'Part Time')),
         'description' => $faker->sentence,
         'salary' => $faker->numberBetween(1000, 10000),
         'county' => $faker->state,
