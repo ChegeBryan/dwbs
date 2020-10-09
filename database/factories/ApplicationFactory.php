4ea21cbd99a9b98dd54d96e5832b9712c7545f25
Chege Bryan <cbbryan8@gmail.com>
2020-10-09 16:19:20 +0300

    Add job applications factory

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Application;
+use Faker\Generator as Faker;
+
+$factory->define(Application::class, function (Faker $faker) {
+    return [
+        'candidate_id' => 2,
+        'job_id' => factory(App\Job::class),
+        'status' => $faker->randomElement($array = array('Filled', 'Open')),
+    ];
+});
