3194d0812f6fa5e8c7489a7d8d69854d54c75cea
Chege Bryan <cbbryan8@gmail.com>
2020-10-10 10:09:38 +0300

    Add candidates profile table

---
+++ database/migrations/2020_10_10_065938_create_candidate_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidate_profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('candidate_id');
+            $table->string('category'); // Shamba boy / House help
+            $table->string('type'); // fulltime/parttime
+            $table->double('salary', 10, 2);
+            $table->string('county');
+            $table->string('town');
+            $table->boolean('status')->default(0); // 0 - Open for Work / Not open for work
+            $table->$table->timestamps();
+            $table->foreign('candidate_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidate_profiles');
+    }
+}
