9eae1af153b9650b4ed6972b7639f0aab251ca38
Chege Bryan <cbbryan8@gmail.com>
2020-10-10 12:29:59 +0300

    Add logout link to sidenav

--- resources/views/candidate/partials/sidenav.blade.php
+++ resources/views/candidate/partials/sidenav.blade.php
@@ -20,8 +20,12 @@
 
   <ul class="list-group list-group-flush border-top-0">
     <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary" href="#"> <i
+         class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('logout') }}"
+         onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> <i
            class="fas fa-sign-out-alt fa-fw mx-2 text-danger"></i> Logout</a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
 
     </li>
     <li class="list-group-item list-group-item-action border-0"><a
