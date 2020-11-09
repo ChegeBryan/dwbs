8a0bd6887fbe68b5a71dcc18eb5bc888bb805474
Chege Bryan <cbbryan8@gmail.com>
2020-11-09 18:23:26 +0300

    Align employer sidenav link names

--- resources/views/employer/partials/sidenav.blade.php
+++ resources/views/employer/partials/sidenav.blade.php
@@ -22,7 +22,7 @@
            class="fas fa-briefcase fa-fw mx-2 text-primary"></i> Manage Jobs</a></li>
     <li class="list-group-item list-group-item-action"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.closed') }}"><i
-           class="fas fa-clipboard-list fa-fw mx-2 text-primary"></i>Closed Jobs</a></li>
+           class="fas fa-clipboard-list fa-fw mx-2 text-primary"></i> Closed Jobs</a></li>
   </ul>
 
   <ul class="list-group list-group-flush border-top-0">
