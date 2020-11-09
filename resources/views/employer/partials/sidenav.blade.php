916a59b99aa22ae99d51724467d484db0cc0f4cf
Chege Bryan <cbbryan8@gmail.com>
2020-11-09 18:36:54 +0300

    Remove dashboard link from employer side nav

--- resources/views/employer/partials/sidenav.blade.php
+++ resources/views/employer/partials/sidenav.blade.php
@@ -6,15 +6,6 @@
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary" href="#"> <i
-           class="fas fa-tachometer-alt fa-fw mx-2 text-primary"></i> Dashboard</a>
-    </li>
-    <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary"
-         href="{{ route('account.edit', Auth::user()->id)}}"><i class="fas fa-user-edit fa-fw mx-2 text-primary"></i>
-        Edit
-        Profile</a></li>
-    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.create') }}"><i
            class="fas fa-user-plus fa-fw mx-2 text-primary"></i> Post New Job</a></li>
     <li class="list-group-item list-group-item-action border-0"><a
@@ -26,6 +17,11 @@
   </ul>
 
   <ul class="list-group list-group-flush border-top-0">
+    <li class="list-group-item list-group-item-action border-0"><a
+         class="pl-2 stretched-link text-decoration-none text-secondary"
+         href="{{ route('account.edit', Auth::user()->id)}}"><i class="fas fa-user-edit fa-fw mx-2 text-primary"></i>
+        Edit
+        Profile</a></li>
     <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="#"
          onclick="event.preventDefault(); document.getElementById('delete_account').submit();"><i
