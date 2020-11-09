bc9d07b5e5f515ee35765ec53d657f3de699b423
Chege Bryan <cbbryan8@gmail.com>
2020-11-09 18:39:24 +0300

    Remove dashboard link from candidate sidenav

--- resources/views/candidate/partials/sidenav.blade.php
+++ resources/views/candidate/partials/sidenav.blade.php
@@ -7,19 +7,15 @@
 
   <ul class="list-group list-group-flush">
     <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('candidate.index')}}"> <i
-           class="fas fa-tachometer-alt fa-fw mx-2 text-danger"></i> Dashboard</a>
-    </li>
-    <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary"
-         href="{{route('candidate.edit', Auth::user()->id)}}"><i class="fas fa-edit fa-fw mx-2 text-danger"></i> Job
-        Preferences</a></li>
+         class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('candidate.index') }}"><i
+           class="fas fa-briefcase fa-fw mx-2 text-danger"></i> Applied Jobs</a></li>
     <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('application.index') }}"><i
            class="fas fa-clipboard-list fa-fw mx-2 text-danger"></i> Jobs Catalog</a></li>
     <li class="list-group-item list-group-item-action border-0"><a
-         class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('candidate.index') }}"><i
-           class="fas fa-briefcase fa-fw mx-2 text-danger"></i> Applied Jobs</a></li>
+         class="pl-2 stretched-link text-decoration-none text-secondary"
+         href="{{route('candidate.edit', Auth::user()->id)}}"><i class="fas fa-edit fa-fw mx-2 text-danger"></i> Job
+        Preferences</a></li>
   </ul>
 
   <ul class="list-group list-group-flush border-top-0">
