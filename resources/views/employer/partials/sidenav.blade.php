d1596f9b537f5fcd79d2b666c06e7b4190a82462
Chege Bryan <cbbryan8@gmail.com>
2020-11-09 17:45:03 +0300

    Add delete and logout links on employer sidenav

--- resources/views/employer/partials/sidenav.blade.php
+++ resources/views/employer/partials/sidenav.blade.php
@@ -11,7 +11,8 @@
     </li>
     <li class="list-group-item list-group-item-action"><a
          class="pl-2 stretched-link text-decoration-none text-secondary"
-         href="{{ route('account.edit', Auth::user()->id)}}"><i class="fas fa-edit fa-fw mx-2 text-primary"></i> Edit
+         href="{{ route('account.edit', Auth::user()->id)}}"><i class="fas fa-user-edit fa-fw mx-2 text-primary"></i>
+        Edit
         Profile</a></li>
     <li class="list-group-item list-group-item-action"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.create') }}"><i
@@ -22,5 +23,23 @@
     <li class="list-group-item list-group-item-action"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.closed') }}"><i
            class="fas fa-clipboard-list fa-fw mx-2 text-primary"></i>Closed Jobs</a></li>
+  </ul>
+
+  <ul class="list-group list-group-flush border-top-0">
+    <li class="list-group-item list-group-item-action border-0"><a
+         class="pl-2 stretched-link text-decoration-none text-secondary" href="#"
+         onclick="event.preventDefault(); document.getElementById('delete_account').submit();"><i
+           class="fas fa-trash fa-fw mx-2 text-primary"></i> Delete Account</a>
+      <form id="delete_account" action="{{ route('account.destroy', Auth::user()->id) }}" method="POST"
+            style="display: none;">
+        @csrf
+        @method('DELETE')
+      </form>
+    </li>
+    <li class="list-group-item list-group-item-action border-0"><a
+         class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('logout') }}"
+         onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> <i
+           class="fas fa-sign-out-alt fa-fw mx-2 text-primary"></i> Logout</a>
+    </li>
   </ul>
 </div>
