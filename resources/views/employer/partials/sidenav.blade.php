8ede07c89d7b78889b8f6df2104915d7ce6d48f5
Chege Bryan <cbbryan8@gmail.com>
2020-11-09 18:26:30 +0300

    Remove borders on employer sidenav

--- resources/views/employer/partials/sidenav.blade.php
+++ resources/views/employer/partials/sidenav.blade.php
@@ -5,22 +5,22 @@
     <h6 class="card-subtitle">{{ ucwords(Auth::user()->mobile) }}</h6>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item list-group-item-action"><a
+    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="#"> <i
            class="fas fa-tachometer-alt fa-fw mx-2 text-primary"></i> Dashboard</a>
     </li>
-    <li class="list-group-item list-group-item-action"><a
+    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary"
          href="{{ route('account.edit', Auth::user()->id)}}"><i class="fas fa-user-edit fa-fw mx-2 text-primary"></i>
         Edit
         Profile</a></li>
-    <li class="list-group-item list-group-item-action"><a
+    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.create') }}"><i
            class="fas fa-user-plus fa-fw mx-2 text-primary"></i> Post New Job</a></li>
-    <li class="list-group-item list-group-item-action"><a
+    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.index') }}"><i
            class="fas fa-briefcase fa-fw mx-2 text-primary"></i> Manage Jobs</a></li>
-    <li class="list-group-item list-group-item-action"><a
+    <li class="list-group-item list-group-item-action border-0"><a
          class="pl-2 stretched-link text-decoration-none text-secondary" href="{{ route('jobs.closed') }}"><i
            class="fas fa-clipboard-list fa-fw mx-2 text-primary"></i> Closed Jobs</a></li>
   </ul>
