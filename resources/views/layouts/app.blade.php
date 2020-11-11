bac9212ce8298a76193f028c9ad7f92bfdf585c4
Chege Bryan <cbbryan8@gmail.com>
2020-11-11 17:54:21 +0300

    Add profile link to logout navbar dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,9 @@
                   {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <div class="dropdown-menu dropdown-menu-right mt-3" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item"
+                     href="@if(Auth::user()->is_employer == 1){{ route('employer') }}@else{{ route('candidate') }}@endif">{{ __('Profile') }}</a>
                   <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
