4087e50ed62a5e23e40d4fbf32eb5d997a99db43
Chege Bryan <cbbryan8@gmail.com>
2020-11-12 09:39:49 +0300

    Update home page cover image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 @section('custom_css')
 <style>
 .bg {
-  background-image: url("{{asset('img/bg.jpeg')}}");
-  height: 80vh;
+  background-image: url("{{asset('img/cover.png')}}");
+  height: 60vh;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
@@ -13,36 +13,9 @@
 @endsection
 
 @section('content')
+<div class="bg">
+</div>
 <div class="container-fluid">
-  <div class="row justify-content-center align-items-center bg">
-    <div class="col-md-8">
-      <h1>Find your preferred domestic work in minutes.</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua.</p>
-      <form>
-        <div class="form-group">
-          <input type="text" class="form-control" id="keywords" placeholder="Keyword e.g. (Job Title, Description)">
-        </div>
-        <div class="form-row">
-          <div class="form-group col">
-            <select id="category" class="custom-select">
-              <option selected disabled>Category</option>
-              <option>Househelp boy</option>
-              <option>Shamba boy</option>
-            </select>
-          </div>
-          <div class="form-group col">
-            <select id="location" class="custom-select">
-              <option selected>Select location</option>
-              <option>Location 1</option>
-              <option>Location 2</option>
-            </select>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">Search</button>
-      </form>
-    </div>
-  </div>
   <div class="d-flex justify-content-center mt-n4"><button class="btn btn-primary btn-lg rounded-circle"><i
          class="fas fa-angle-double-down fa-fw"></i></button></div>
 </div>
